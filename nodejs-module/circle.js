ee1d00934d3529ca16d7dfe9baec8d0f5e3e7c51
Pham Tuan <pham.thanh.tuan@framgia.com>
2018-04-13 17:25:31 +0700

    Node.js module system

---
+++ nodejs-module/circle.js
@@ -0,0 +1,3 @@
+const { PI } = Math; // Extract PI from Math.PI
+
+exports.area = (r) => PI * (r ** 2);
---
+++ nodejs-module/foo.js
@@ -0,0 +1,8 @@
+const circle = require('./circle.js');
+console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
+
+const Square = require('./square.js');
+const mySquare = new Square(2);
+console.log(`The area of mySquare is ${mySquare.area()}`);
+
+
